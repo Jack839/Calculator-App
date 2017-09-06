582b06228f77501f5c4ef15ef37dc743c85ca885
Jeevant Sah <jeev83840@gmail.com>
2017-09-06 22:30:22 +0530

    New Update
    -Updated To Version 1.0.1.
    -Updated layout from box to anchor.
    -Changed TextInput to Label for making it non editable.
    -Updated Icon to give material design.
    -Removed  importing of whole Kivy library.
    -Added ObjectProperty for calling text in Label.

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
-import kivy
 import __future__
 
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
 from kivy.core.window import Window
+from kivy.properties import StringProperty
 
 class CalcLayout(FloatLayout):
     Window.clearcolor = (1, 1, 1, 1)
+    data_text=StringProperty()
     def valEnter(self,inputVal):
         if inputVal=="":
-            self.data.text=""
-        elif self.data.text=="Error":
-            self.data.text=inputVal
+            self.data_text=""
+        elif self.data_text=="Error":
+            self.data_text=inputVal
         else:
-            self.data.text+=inputVal
+            self.data_text+=inputVal
     def calc(self,valToEval):
         try:
-            self.data.text=str(eval(compile(valToEval, '<string>', 'eval', __future__.division.compiler_flag)))
+            self.data_text=str(eval(compile(valToEval, '<string>', 'eval', __future__.division.compiler_flag)))
         except:
-            self.data.text="Error"
+            self.data_text="Error"
 class CalculatorApp(App):
     def build(self):
         return CalcLayout()
